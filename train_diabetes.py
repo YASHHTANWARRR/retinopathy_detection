5264b0cd5a1a0007ab18143a1496b30b41792e00
YASHHTANWARRR <yashtanwar11092005@gmail.com>
2026-04-18 01:11:17 +0530

    error removal

--- train_diabetes.py
+++ train_diabetes.py
@@ -1,9 +1,9 @@
 import os
-import cv2
 import torch
 import cudf
 import numpy as np
 import torch.nn as nn
+from PIL import Image
 from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, models
@@ -19,9 +19,18 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 data = []
 
 for label in os.listdir(DATA_PATH):
+    if not label.isdigit():
+        continue
+
     folder = os.path.join(DATA_PATH, label)
+
     for img in os.listdir(folder):
-        data.append([os.path.join(folder, img), int(label)])
+        img_path = os.path.join(folder, img)
+
+        if not os.path.isfile(img_path):
+            continue
+
+        data.append([img_path, int(label)])
 
 df = cudf.DataFrame(data, columns=["image", "label"])
 df = df.to_pandas()
@@ -40,8 +49,7 @@ class RetinoDataset(Dataset):
         img_path = self.df.loc[idx, "image"]
         label = self.df.loc[idx, "label"]
 
-        image = cv2.imread(img_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.open(img_path).convert("RGB")
 
         if self.transform:
             image = self.transform(image)
@@ -49,7 +57,6 @@ class RetinoDataset(Dataset):
         return image, label
 
 train_transform = transforms.Compose([
-    transforms.ToPILImage(),
     transforms.Resize((224, 224)),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(10),
@@ -57,7 +64,6 @@ train_transform = transforms.Compose([
 ])
 
 val_transform = transforms.Compose([
-    transforms.ToPILImage(),
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
